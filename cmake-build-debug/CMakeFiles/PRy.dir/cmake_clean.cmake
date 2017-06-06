1bca79e649e182e628bf03bf3603717f6b8ad1e8
Michał Wójcik <michal.p.wojcik@student.put.poznan.pl>
2017-06-06 16:54:55 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/PRy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRy.dir/main.cpp.o"
+  "CMakeFiles/PRy.dir/ProcessState.cpp.o"
+  "CMakeFiles/PRy.dir/LamportClock.cpp.o"
+  "PRy.pdb"
+  "PRy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
